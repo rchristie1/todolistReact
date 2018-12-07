e44ff9cbe256960f119e916c8dca2d74ffc469fe
System Administrator <root@Ryans-MacBook-Air.local>
2018-12-06 20:53:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import TodoListFull from './TodoList/TodoListFull';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <TodoListFull />
+    //   <div>
+    //     <title>To-Do</title>
+    //     <div className="container">
+    //       <div id="container" className="col-md-8 col-md-offset-2">  </div>
+    //     </div>
+    // </div>
     );
   }
 }
